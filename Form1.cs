4f2c15c01938370f42debb16134dfa6dd35eea9a
Marco Giacomelli <marco.giacomelli@kinship.io>
2024-04-23 10:09:01 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,11 @@
+using Autodesk.Revit.UI;
+namespace FormsCrash
+{
+    public partial class Form1 : frmISearch
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
