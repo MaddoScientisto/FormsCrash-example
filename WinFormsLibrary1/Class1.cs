4e1373f5a7951f6ec3f0ffffbe73f80a854ee6c2
Marco Giacomelli <marco.giacomelli@kinship.io>
2024-04-23 11:07:52 +0200

    Local dll test

---
+++ WinFormsLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace WinFormsLibrary1
+{
+    public class ExternalClass : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize((object) this);
+            GC.KeepAlive((object)this);
+        }
+    }
+}
